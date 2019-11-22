aabc8f6aa31c1bafb6c5ed8a3af88753eceb71ea
Jason <jwhall.webdev@gmail.com>
2019-11-22 01:28:13 -0700

    build update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var pump = require('pump');
 const { pipeline } = require('stream');
 const babel = require('gulp-babel');
 const autoprefixer = require('gulp-autoprefixer');
@@ -17,51 +18,55 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./src/css'));
 });
 
-exports.defualt = () => (
+gulp.task('babel', async () => 
   gulp.src('./src/js/*.js')
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(gulp.dest('./js'))
-);
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(gulp.dest('./js'))
+)
 
-exports.default = () => (
-  pipeline(
-    gulp.src('./js/*.js'),
-    uglify(),
-    gulp.dest('./js')
-  )
-);
+gulp.task('compress', function (cb) {
+  pump([
+      gulp.src('./js/*.js'),
+      uglify(),
+      gulp.dest('./js')
+    ],
+    cb
+  );
+})
 
-exports.default = () => (
+gulp.task('autoprefixer', async () => {
   gulp.src('./src/css/*.css')
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(gulp.dest('./css'))
-);
+  .pipe(autoprefixer({
+    cascade: false
+  }))
+  .pipe(gulp.dest('./css'))
+})
 
-exports.default = () => (
+gulp.task('minify-css', async () => {
   gulp.src('./css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./css'))
-);
+});
 
-exports.default = () => (
+gulp.task('img-compress', async () => {
   gulp.src('./src/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./images'))
-);
+})
 
-exports.default = async () => {
+gulp.task('transfer-index', async () => {
   gulp.src('./src/index.html')
     .pipe(gulp.dest('./'))
-}
+})
 
-exports.default = async () => {
+gulp.task('transfer-music', async () => {
   gulp.src('./src/music/*')
     .pipe(gulp.dest('./music'))
-}
+})
+
+gulp.task('build', gulp.series('babel', 'autoprefixer', 'minify-css', 'img-compress', 'compress', 'transfer-index', 'transfer-music'))
 
 gulp.task('watch', function() {
   gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
